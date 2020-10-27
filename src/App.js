6c4584678eae0c127db21bbdc36499c22f8fbcb8
Sagar Parker <sagar8parker@gmail.com>
2020-10-27 19:00:29 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { useState,useEffect } from 'react';
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import ToDo from './ToDo';
+import Counter from './Counter';
+import Home from './Home';
+
 
 function App(){
-  
-
-
-
-
-  
+  const history = useHistory();
   return(
     <div className="App-header">
-      <ToDo/>
+      <button onClick={()=> history.push("/")}>Home</button>
+      <button onClick={()=> history.push("/counter")}>Counter</button>
+      <button onClick={()=> history.push("/todo")}>ToDo</button>
+
+      <Link to="/">Home</Link>
+      <Link to="/counter">counter</Link>
+      <Link to="/todo">todo</Link>
+      
+      
+
+      <Switch>
+        <Route path="/counter">
+          <Counter/>
+        </Route>
+        <Route path="/todo">
+          <ToDo/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
   )
 }
